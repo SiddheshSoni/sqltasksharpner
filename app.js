13bf01c37295c487c1134d0cbc1eb6102085dfd1
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-05-31 22:01:17 +0530

    sequalize implimentation

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const studentRoute = require('./routes/studentRoute');
+const db = require('./utils/db-connection');
+
+//models
+const StudentModels = require('./models/students');
 
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    resizeBy.send("Hello World!");
+    res.send("Hello World!");
 })
 
 app.use('/students', studentRoute);
 
-app.listen(4000, ()=>{
-    console.log("Server Started at port 4000!");
+db.sync({force:true}).then(()=>{
+    app.listen(4000, ()=>{
+        console.log("Server Started at port 4000!");
+    })
+}).catch((err)=>{
+    console.log(err);
 })
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,33 +1,52 @@
-const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Sid2444',
-    database:'studentdb'
+const sequelize = new Sequelize('studentdb2', 'root', 'Sid2444', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
-        return;
+(async()=>{
+    try {
+      await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
-    console.log("Connection to DB Successfull!");
+})();
 
-    const createStudentTBQuery = `CREATE TABLE IF NOT EXISTS Students(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(20),
-        email VARCHAR(30) UNIQUE,
-        age INT
-    )`;
+module.exports = sequelize;
 
-    connection.execute(createStudentTBQuery, (err)=>{
-        if(err){
-            console.log(err);
-            return;
-        }
-        console.log("Successfully created table Student!");
-    })
-});
 
-module.exports= connection;
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'Sid2444',
+//     database:'studentdb'
+// });
+
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log("Connection to DB Successfull!");
+
+//     const createStudentTBQuery = `CREATE TABLE IF NOT EXISTS Students(
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         name VARCHAR(20),
+//         email VARCHAR(30) UNIQUE,
+//         age INT
+//     )`;
+
+//     connection.execute(createStudentTBQuery, (err)=>{
+//         if(err){
+//             console.log(err);
+//             return;
+//         }
+//         console.log("Successfully created table Student!");
+//     })
+// });
+
+// module.exports= connection;
